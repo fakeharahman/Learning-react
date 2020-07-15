4fa587266502dfad1dff3b8414ee4f480b868e23
Fakeha Rahman <fakeha126@gmail.com>
2020-07-16 01:38:36 +0530

    webpack

---
+++ reactWebpack/src/components/PizzaImage/PizzaImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './PizzaImage.module.css'
+import PizzaImg from '../../assets/pizza.jpg'
+
+const pizzaImage = props => (
+    <div className={classes.PizzaImage}>
+        <img src={PizzaImg} className={classes.PizzaImg} />
+    </div>
+)
+
+export default pizzaImage
